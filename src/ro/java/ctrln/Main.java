a639a49e897d87a7189fa79a7ebb7ebdca3b44ed
PaulaMuntean <91008909+PaulaMuntean@users.noreply.github.com>
2021-11-08 17:06:13 +0200

    Modificare Maine.java adaugare com

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -1,7 +1,7 @@
 package ro.java.ctrln;
 
 public class Main {
-
+//javatortoise
     public static void main(String[] args) {
 	    System.out.println("Invat limbajul Java si despre masina virtiala Java");
     }
