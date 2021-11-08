393f8ee37cd01fe28e80568720e5d94532ea77d1
PaulaMuntean <91008909+PaulaMuntean@users.noreply.github.com>
2021-11-08 16:01:18 +0200

    modificare Maine.java

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -1,7 +1,7 @@
 package ro.java.ctrln;
 
 public class Main {
-    //
+
     public static void main(String[] args) {
 	    System.out.println("Invat limbajul Java si despre masina virtiala Java");
     }
