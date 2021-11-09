59d9ebaf496c59604a6c6b7862b84ade30734022
PaulaMuntean <91008909+PaulaMuntean@users.noreply.github.com>
2021-11-09 10:37:46 +0200

    adaugare comentari pe o singura linie

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -1,8 +1,9 @@
 package ro.java.ctrln;
 
 public class Main {
-
+//acesata este metoda main, care porneste o aplicatie java
     public static void main(String[] args) {
+	   //inainte de comentariu nu se poate pune text , aici vrem sa afisam un mesaj pe consila java
 	    System.out.println("Invat limbajul Java si despre masina virtiala Java");
     }
 }
